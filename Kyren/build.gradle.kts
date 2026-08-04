d8107190653c48ceeeefa372344555dcab001d4f
csksy <saprujatin70@gmail.com>
2026-08-04 11:35:15 +0530

    Update build.gradle.kts

--- Kyren/build.gradle.kts
+++ Kyren/build.gradle.kts
@@ -1,30 +1,6 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-}
-
-val androidSdk = 34
-val javaVersion = JavaVersion.VERSION_1_8
+version = 1
 
 android {
-    namespace = "com.laddu100.kyren"
-    compileSdk = androidSdk
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
     buildFeatures {
         buildConfig = true
     }
