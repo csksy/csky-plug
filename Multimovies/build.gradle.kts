0b50d4c119ff5c56f7eb3b47ea6473834b67ee33
Raghav <159143147+KSHITIJ8473@users.noreply.github.com>
2026-08-12 13:29:49 +0000

    add multimovies

---
+++ Multimovies/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 5
+
+cloudstream {
+    language = "en"
+    description = "Multimovies - Movies, TV Shows & Anime with Cineverse (StreamHG/EarnVids/SeekStreaming/RPMShare/UpnShare/StreamP2P...) + GDMIRROR sources, multi-audio & subtitles"
+    authors = listOf("csksy")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+    iconUrl = "https://multimovies.motorcycles/wp-content/uploads/2024/01/cropped-CompressJPEG.online_512x512_image.png"
+}
